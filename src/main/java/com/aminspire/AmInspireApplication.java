568a181e2801d03c25a656d2296197aa6ce30df4
willjsw <sunwoo1137@gmail.com>
2025-02-18 21:36:05 +0900

    init:initial project

---
+++ src/main/java/com/aminspire/AmInspireApplication.java
@@ -0,0 +1,13 @@
+package com.aminspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmInspireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmInspireApplication.class, args);
+    }
+
+}
