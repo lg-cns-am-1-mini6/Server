46a3b64e5e82d5c48097692e45942a133316de84
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-02-19 18:30:08 +0900

    Revert "Chore/common exception"

--- src/main/java/com/aminspire/global/exception/ErrorResponse.java
+++
@@ -1,7 +0,0 @@
-package com.aminspire.global.exception;
-
-public record ErrorResponse(String errorClassName, String message) {
-    public static ErrorResponse of(String errorClassName, String message) {
-        return new ErrorResponse(errorClassName, message);
-    }
-}
