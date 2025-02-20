794229d1f17303590128fab7689816ee5c1ff1e7
woogieon8on <swp0927@naver.com>
2025-02-20 15:59:25 +0900

    #12 feat: Redis 설정 추가

---
+++ src/main/java/com/aminspire/infra/config/redis/RedisClient.java
@@ -0,0 +1,54 @@
+package com.aminspire.infra.config.redis;
+
+import com.aminspire.global.exception.CommonException;
+import com.aminspire.global.exception.errorcode.RedisErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisClient {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void setValue(String key, String value, Long timeout) {
+        try {
+            ValueOperations<String, Object> values = redisTemplate.opsForValue();
+            values.set(key, value, Duration.ofMillis(timeout));
+        } catch (Exception e) {
+            throw new CommonException(RedisErrorCode.REDIS_ERROR);
+        }
+    }
+
+    public String getValue(String key) {
+        try {
+            ValueOperations<String, Object> values = redisTemplate.opsForValue();
+            if (values.get(key) == null) {
+                return "";
+            }
+            return values.get(key).toString();
+        } catch (Exception e) {
+            throw new CommonException(RedisErrorCode.REDIS_ERROR);
+        }
+    }
+
+    public void deleteValue(String key) {
+        try {
+            redisTemplate.delete(key);
+        } catch (Exception e) {
+            throw new CommonException(RedisErrorCode.REDIS_ERROR);
+        }
+    }
+
+    public boolean checkExistsValue(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            throw new CommonException(RedisErrorCode.REDIS_ERROR);
+        }
+    }
+}
