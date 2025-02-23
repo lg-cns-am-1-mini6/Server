ea02baf5418cc8acf8cc2699f201c5fe1e314e93
woogieon8on <swp0927@naver.com>
2025-02-24 00:00:06 +0900

    #40 refactor: 서비스 인터페이스 및 구현체 구현

--- src/main/java/com/aminspire/domain/user/controller/AuthController.java
+++ src/main/java/com/aminspire/domain/user/controller/AuthController.java
@@ -2,8 +2,8 @@ package com.aminspire.domain.user.controller;
 
 import com.aminspire.domain.user.dto.response.LoginResponse;
 import com.aminspire.domain.user.dto.response.TokenResponse;
-import com.aminspire.domain.user.service.AuthService;
-import com.aminspire.domain.user.service.SocialLoginService;
+import com.aminspire.domain.user.service.AuthServiceImpl;
+import com.aminspire.domain.user.service.SocialLoginServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,28 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final SocialLoginService socialLoginService;
-    private final AuthService authService;
+    private final SocialLoginServiceImpl socialLoginServiceImpl;
+    private final AuthServiceImpl authServiceImpl;
 
     @PostMapping("/google/sign-in")
     public LoginResponse signInWithGoogle(
             @RequestParam("code") String code, HttpServletResponse response) {
-        return socialLoginService.signInWithGoogle(code, response);
+        return socialLoginServiceImpl.signInWithGoogle(code, response);
     }
 
     @PostMapping("/kakao/sign-in")
     public LoginResponse signInWithKakao(
             @RequestParam("code") String code, HttpServletResponse response) {
-        return socialLoginService.signInWithKakao(code, response);
+        return socialLoginServiceImpl.signInWithKakao(code, response);
     }
 
     @PostMapping("/reissue")
     public TokenResponse reissue(HttpServletRequest request, HttpServletResponse response) {
-        return authService.recreate(request, response);
+        return authServiceImpl.recreate(request, response);
     }
 
     @PostMapping("/sign-out")
     public TokenResponse signOut(HttpServletRequest request, HttpServletResponse response) {
-        return authService.signOut(request, response);
+        return authServiceImpl.signOut(request, response);
     }
 }
--- src/main/java/com/aminspire/domain/user/service/AuthService.java
+++ src/main/java/com/aminspire/domain/user/service/AuthService.java
@@ -1,79 +1,12 @@
 package com.aminspire.domain.user.service;
 
-import com.aminspire.domain.user.domain.user.User;
 import com.aminspire.domain.user.dto.response.TokenResponse;
-import com.aminspire.global.exception.CommonException;
-import com.aminspire.global.exception.errorcode.JwtErrorCode;
-import com.aminspire.global.exception.errorcode.UserErrorCode;
-import com.aminspire.global.security.jwt.JwtProvider;
-import com.aminspire.infra.config.redis.RedisClient;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
-@Service
-@RequiredArgsConstructor
-public class AuthService {
+public interface AuthService {
 
-    private final JwtProvider jwtProvider;
-    private final RedisClient redisClient;
+    TokenResponse recreate(HttpServletRequest request, HttpServletResponse response);
 
-    public TokenResponse recreate(HttpServletRequest request, HttpServletResponse response) {
-
-        String refreshToken = jwtProvider.getRefreshTokenFromCookie(request);
-        boolean isValid = jwtProvider.validateToken(refreshToken, "refresh");
-
-        if (!isValid) {
-            throw new CommonException(JwtErrorCode.REFRESH_TOKEN_INVALID);
-        }
-
-        String email = jwtProvider.getEmail(refreshToken);
-        String redisRefreshToken = redisClient.getValue(email);
-
-        if (StringUtils.isEmpty(refreshToken) || StringUtils.isEmpty(redisRefreshToken) || !redisRefreshToken.equals(refreshToken)) {
-            throw new CommonException(JwtErrorCode.REFRESH_TOKEN_INVALID);
-        }
-
-        jwtProvider.recreate(refreshToken, response);
-
-        return TokenResponse.of("엑세스 토큰 재발급 성공");
-    }
-
-    @Transactional
-    public TokenResponse signOut(HttpServletRequest request, HttpServletResponse response) {
-
-        String accessToken = jwtProvider.getAccessTokenFromRequest(request);
-        String refreshToken = jwtProvider.getRefreshTokenFromCookie(request);
-
-        if(refreshToken == null || accessToken == null){
-            throw new CommonException(JwtErrorCode.TOKEN_NOT_FOUND);
-        }
-
-        if (!jwtProvider.validateToken(accessToken, "access")) {
-            throw new CommonException(JwtErrorCode.ACCESS_TOKEN_INVALID);
-        }
-
-        if (!jwtProvider.validateToken(accessToken, "refresh")) {
-            throw new CommonException(JwtErrorCode.REFRESH_TOKEN_INVALID);
-        }
-
-        redisClient.deleteValue(jwtProvider.getEmail(accessToken));
-
-        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", null)
-                .maxAge(0)
-                .secure(true)
-                .path("/")
-                .httpOnly(true)
-                .sameSite("Strict")
-                .build();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
-
-        return TokenResponse.of("로그아웃 성공");
-    }
+    TokenResponse signOut(HttpServletRequest request, HttpServletResponse response);
 }
