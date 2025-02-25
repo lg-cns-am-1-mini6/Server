0aa2eb5ea64ec82c630ab07f64f5d4c8e267a2e0
woogieon8on <swp0927@naver.com>
2025-02-25 12:43:25 +0900

    #56 fix: 쿠키 sameSite 설정 None으로 변경

--- src/main/java/com/aminspire/global/security/jwt/JwtProvider.java
+++ src/main/java/com/aminspire/global/security/jwt/JwtProvider.java
@@ -124,7 +124,7 @@ public class JwtProvider {
                 .secure(true)
                 .path("/")
                 .httpOnly(true)
-                .sameSite("Strict")
+                .sameSite("None")
                 .build();
     }
 
